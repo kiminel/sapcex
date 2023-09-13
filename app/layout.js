95408e80be49b1ed56e12b8d479fb31cdfe6edd3
Kimi Nel <kimi.monique.nel@gmail.com>
2023-09-13 12:32:27 +0200

    style: adding ThemeProvider

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 import { Inter } from "next/font/google";
 import { Box, Button, Container, Typography } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
@@ -21,6 +23,23 @@ export default function RootLayout({ children }) {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const apiUrl = "https://api.spacexdata.com/v4/launches/query";
+
+  const appTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#FFFFFF",
+      },
+      action: {
+        disabledBackground: "#949494",
+        disabled: "#d3d3d3",
+      },
+    },
+    typography: {
+      h1: { fontSize: "8rem", fontWeight: 600 },
+      h2: { fontSize: "6rem" },
+      h3: { fontSize: "4rem" },
+    },
+  });
 
   const theme = useTheme();
   const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -119,11 +138,10 @@ export default function RootLayout({ children }) {
       });
 
       if (!response.ok) {
-        console.log("Network response was not ok");
+        console.error("Network failure");
       }
 
       const responseData = await response.json();
-      console.log(responseData);
       setData(responseData);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -157,140 +175,142 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Box sx={{ height: 1, width: 1, bgcolor: "black" }}>
-          {data["docs"] ? (
-            <>
-              <Header />
+        <ThemeProvider theme={appTheme}>
+          <Box sx={{ height: 1, width: 1, bgcolor: "black" }}>
+            {data["docs"] ? (
+              <>
+                <Header />
 
-              <Box sx={{ position: "relative" }}>
-                <>
-                  <Box
-                    component="img"
-                    sx={{
-                      width: 1,
-                      height: 1,
-                      paddingBottom: "12px",
-                      paddingTop: isMediumScreen ? "55px" : "0px",
-                    }}
-                    alt="Launch cover"
-                    src={data["docs"][0].rocket.flickr_images[4]}
-                  />
-
-                  {!isMediumScreen && (
+                <Box sx={{ position: "relative" }}>
+                  <>
                     <Box
+                      component="img"
                       sx={{
-                        position: "absolute",
-                        top: "30%",
-                        left: "15%",
-                        color: "white",
+                        width: 1,
+                        height: 1,
+                        paddingBottom: "12px",
+                        paddingTop: isMediumScreen ? "55px" : "0px",
                       }}
-                    >
-                      <CountOnScroll numberToCount={data["totalDocs"]} />
-                      <Typography
+                      alt="Launch cover"
+                      src={data["docs"][0].rocket.flickr_images[4]}
+                    />
+
+                    {!isMediumScreen && (
+                      <Box
                         sx={{
-                          textTransform: "uppercase",
-                          display: "flex",
-                          justifyContent: "center",
+                          position: "absolute",
+                          top: "30%",
+                          left: "15%",
+                          color: "white",
                         }}
                       >
-                        Total SpaceX Launches
-                      </Typography>
-                    </Box>
-                  )}
-                </>
-              </Box>
-
-              {isMediumScreen && (
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    py: 4,
-                    color: "white",
-                  }}
-                >
-                  <CountOnScroll numberToCount={data["totalDocs"]} />
-                  <Typography sx={{ textTransform: "uppercase" }}>
-                    Total SpaceX Launches
-                  </Typography>
+                        <CountOnScroll numberToCount={data["totalDocs"]} />
+                        <Typography
+                          sx={{
+                            textTransform: "uppercase",
+                            display: "flex",
+                            justifyContent: "center",
+                          }}
+                        >
+                          Total SpaceX Launches
+                        </Typography>
+                      </Box>
+                    )}
+                  </>
                 </Box>
-              )}
 
-              <Typography
-                variant={variant}
-                sx={{
-                  color: "white",
-                  textAlign: "center",
-                  letterSpacing: "12px",
-                  pt: 4,
-                  pb: 12,
-                }}
-              >
-                <BouncingWord text={"Explore"} />
-              </Typography>
-
-              <Container maxWidth={false} sx={{ bgcolor: "black" }}>
-                <Box sx={{ flexDirection: "column" }}>
-                  <Box ref={launchesRef}>
-                    <Launches launches={data["docs"]} />
-                  </Box>
-
+                {isMediumScreen && (
                   <Box
                     sx={{
                       display: "flex",
-                      flexDirection: "column",
                       alignItems: "center",
+                      flexDirection: "column",
+                      py: 4,
                       color: "white",
-                      py: 2,
-                      gap: 1,
                     }}
                   >
-                    <Typography>
-                      Page {data["page"]} / {data["totalPages"]}{" "}
+                    <CountOnScroll numberToCount={data["totalDocs"]} />
+                    <Typography sx={{ textTransform: "uppercase" }}>
+                      Total SpaceX Launches
                     </Typography>
+                  </Box>
+                )}
 
-                    <Box sx={{ display: "flex", gap: 2, py: 4 }}>
-                      <Button
-                        variant="contained"
-                        onClick={prevPage}
-                        disabled={currentPage === 1}
-                        sx={{ bgcolor: "white", color: "black" }}
-                      >
-                        Prev Page
-                      </Button>
+                <Typography
+                  variant={variant}
+                  sx={{
+                    color: "white",
+                    textAlign: "center",
+                    letterSpacing: "12px",
+                    pt: 4,
+                    pb: 12,
+                  }}
+                >
+                  <BouncingWord text={"Explore"} />
+                </Typography>
 
-                      <Button
-                        variant="contained"
-                        onClick={nextPage}
-                        disabled={currentPage === data["totalPages"]}
-                        sx={{ bgcolor: "white", color: "black" }}
-                      >
-                        Next Page
-                      </Button>
+                <Container maxWidth={false} sx={{ bgcolor: "black" }}>
+                  <Box sx={{ flexDirection: "column" }}>
+                    <Box ref={launchesRef}>
+                      <Launches launches={data["docs"]} />
+                    </Box>
+
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        color: "white",
+                        py: 2,
+                        gap: 1,
+                      }}
+                    >
+                      <Typography>
+                        Page {data["page"]} / {data["totalPages"]}{" "}
+                      </Typography>
+
+                      <Box sx={{ display: "flex", gap: 2, py: 4 }}>
+                        <Button
+                          variant="contained"
+                          onClick={prevPage}
+                          disabled={currentPage === 1}
+                          sx={{ bgcolor: "white", color: "black" }}
+                        >
+                          Prev Page
+                        </Button>
+
+                        <Button
+                          variant="contained"
+                          onClick={nextPage}
+                          disabled={currentPage === data["totalPages"]}
+                          sx={{ bgcolor: "white", color: "black" }}
+                        >
+                          Next Page
+                        </Button>
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-              </Container>
+                </Container>
 
-              <Footer />
-            </>
-          ) : (
-            <Box
-              sx={{
-                width: 1,
-                height: "100vh",
-                bgcolor: "black",
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Typography variant="h3" sx={{ color: "white", p: 40 }}>
-                Loading...
-              </Typography>
-            </Box>
-          )}
-        </Box>
+                <Footer />
+              </>
+            ) : (
+              <Box
+                sx={{
+                  width: 1,
+                  height: "100vh",
+                  bgcolor: "black",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography variant="h3" sx={{ color: "white", p: 40 }}>
+                  Loading...
+                </Typography>
+              </Box>
+            )}
+          </Box>
+        </ThemeProvider>
       </body>
     </html>
   );
